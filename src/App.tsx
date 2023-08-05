8cd3ccf56531068a17055cdb0ffe5a75425f9e46
Nur Cahyanto <nur.razdan@gmail.com>
2023-08-05 22:28:53 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import Layoutt from "./layout/Layout";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import DetailPage from "./pages/DetailPage";
 
 const App: React.FC = () => {
   return (
     <div>
-      <Home/>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/detail" element={<DetailPage />} />
+      </Routes>
+      </BrowserRouter>
+     
     </div>
   );
 };
