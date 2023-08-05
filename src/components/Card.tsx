f33bb4178c6134038aecc92611246d6786638900
Nur Cahyanto <nur.razdan@gmail.com>
2023-08-05 22:28:22 +0700

    Update Card.tsx

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-import { Card} from "antd";
+import { Card, Image} from "antd";
 
 const { Meta } = Card;
 
@@ -7,24 +7,32 @@ interface props{
     image: string;
     title: string;
     description: string
+    author: string
+    key: number
 
 }
-const Cardd:FC<props> = ({image, title, description}) => {
+const Cardd:FC<props> = ({image, title, description, author, key}) => {
   return (
     <div>
         <Card
-           className="w-[300] max-w-sm transform hover:scale-110 transition-transform duration-500 hover:z-30"
+           className="w-[300] min-h-[300] max-w-sm transform hover:scale-110 transition-transform duration-500 hover:z-20 cursor-pointer"
             cover={
-              <img
-                alt="image Cover"
-                src={image}
-              />
+              <Image
+  
+    src={image}
+    fallback="https://tse3.mm.bing.net/th?id=OIP.FjLkalx51D8xJcpixUGJywHaE8&pid=Api&P=0&h=180"/>
+             
             }
           >
             <Meta
               title={title}
               description={description}
+              
             />
+            <div className="w-full flex justify-end mt-5" key={key}>
+              {author}
+
+            </div>
           </Card>
     </div>
   )
